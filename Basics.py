3e5e9db6d7b63a27eb2bca2e9ce663072e165c09
vikash choudhari <vikash.choudhari07@gmail.com>
2019-11-30 15:46:33 +0530

    datetime and math module example

--- Basics.py
+++ Basics.py
@@ -1,4 +1,6 @@
 import sys
+import datetime
+from math import pi
 class Basic:
     def __init__(self):
         print('ooh la la, here we go...')
@@ -14,12 +16,32 @@ class Basic:
         print('version info')
         print(sys.version_info)
 
+    def get_time(self):
+        now = datetime.datetime.now()
+        print('Current date and time is: ')
+        print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+    def calc_area(self,radius):
+        print('Area of circle with given radius ' + str(radius) + ' is: ' + str(pi * radius**2))
+
+    def reverse_name(self):
+        fname = input('Enter first name: ')
+        lname = input('Enter last name: ')
+        print('Hello ' + lname + ' ' + fname)
+
 if __name__ == '__main__':
     b = Basic()
     print('###...print twinkle twinkle start...###')
     b.print_twinkle()
     print('###...print python version...###')
     b.get_version()
+    print('###...print current date and time...###')
+    b.get_time()
+    print('###...calculate area of circle...###')
+    rad = float(input('Provide the radius of circle...'))
+    b.calc_area(rad)
+    print('###...reverse last name and first name...###')
+    b.reverse_name()
 
 
 
