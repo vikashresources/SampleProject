997f3226fe3f012af340e8a8db1aa61d3176ae51
vikash choudhari <vikash.choudhari07@gmail.com>
2019-11-30 14:52:17 +0530

    Basics python practice program

---
+++ Basics.py
@@ -0,0 +1,19 @@
+class Basic:
+    def __init__(self):
+        print("ooh la la, here we go...")
+
+    def print_twinkle(self):
+        print(
+            "Twinkle, twinkle, little star, \n\tHow I wonder what you are! "
+            "\n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. "
+            "\nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+
+if __name__ == '__main__':
+    b = Basic()
+    b.print_twinkle()
+
+
+
+
+
+
