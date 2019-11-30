886c453c3a66404a4dd677c953b7cc83f9f70a81
vikash choudhari <vikash.choudhari07@gmail.com>
2019-11-30 15:15:53 +0530

    Basics python practice program

--- Basics.py
+++ Basics.py
@@ -1,16 +1,25 @@
+import sys
 class Basic:
     def __init__(self):
-        print("ooh la la, here we go...")
+        print('ooh la la, here we go...')
 
     def print_twinkle(self):
         print(
             "Twinkle, twinkle, little star, \n\tHow I wonder what you are! "
             "\n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. "
             "\nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+    def get_version(self):
+        print('python version:')
+        print(sys.version)
+        print('version info')
+        print(sys.version_info)
 
 if __name__ == '__main__':
     b = Basic()
+    print('###...print twinkle twinkle start...###')
     b.print_twinkle()
+    print('###...print python version...###')
+    b.get_version()
 
 
 
