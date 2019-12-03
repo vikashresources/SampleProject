08032e2039f813c078e4d36334f3d556e13e2d60
vikash choudhari <vikash.choudhari07@gmail.com>
2019-12-03 23:15:01 +0530

    Fixex

--- StackBalancedParenthesis.py
+++ StackBalancedParenthesis.py
@@ -10,43 +10,47 @@ from Stack import Stack
 
 }
 
-
-
 '''
 
 
-def is_match(pop, paren):
-    if pop == '(' and paren == ')':
-        return True
-    elif pop == '{' and paren == '}':
-        return True
-    elif pop == '[' and paren == ']':
-        return True
-    else:
-        return False
+class StackBalancedParenthesis:
 
-
-def is_parenthesis_balanced(pattern_string):
-    s = Stack()
-    is_balanced = True
-    index = 0
-    while index < len(pattern_string) and is_balanced:
-        paren = pattern_string[index]
-        if paren in "{[(":
-            s.push(paren)
+    def is_match(self, pop, paren):
+        if pop == '(' and paren == ')':
+            return True
+        elif pop == '{' and paren == '}':
+            return True
+        elif pop == '[' and paren == ']':
+            return True
         else:
-            if s.is_empty():
-                is_balanced = False
+            return False
+
+    def is_parenthesis_balanced(self, pattern_string):
+        s = Stack()
+        is_balanced = True
+        index = 0
+        while index < len(pattern_string) and is_balanced:
+            paren = pattern_string[index]
+            if paren in "{[(":
+                s.push(paren)
             else:
-                top = s.pop()
-                if not is_match(top, paren):
+                if s.is_empty():
                     is_balanced = False
-    index += 1
+                else:
+                    top = s.pop()
+                    if not self.is_match(top, paren):
+                        is_balanced = False
 
-    if s.empty() and is_balanced:
-        return True
-    else:
-        return False
+            index += 1
+
+        if s.is_empty() and is_balanced:
+            return True
+        else:
+            return False
 
 
-print(is_parenthesis_balanced("()"))
+if __name__ == '__main__':
+    sbp = StackBalancedParenthesis()
+    print(sbp.is_parenthesis_balanced('(())'))
+    print(sbp.is_parenthesis_balanced('(()'))
+    print(sbp.is_parenthesis_balanced('(({[]}))'))
